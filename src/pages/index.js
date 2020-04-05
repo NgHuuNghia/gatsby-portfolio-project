9f0ab1d8f343c115ea4b6422d246714611aab4d9
Nguyễn Hữu Nghĩa <nguyenhuunghia152@gmail.com>
2020-04-05 14:15:05 +0700

    basic content intro and portfolio

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,228 @@
 import React from "react"
-import Layout from '../components/layout'
+import Layout from "../components/layout"
+import port1 from "../images/portfolio/gb-blog-template.png"
 
 const IndexPage = () => {
     return (
         <Layout>
-            <div className='main'>
-                <div className='intro' id='intro'>
-                    <div className='content-intro'>
-                        <h1 className='title'>I'M NGUYEN HUU NGHIA.</h1>
-                        <p>Software Engineer, Web Developer, JavaScript Lover & Blogger @nguyenhuunghia</p>
-                        <div style={{ marginTop: '4rem' }}>
-                            <button className='btn btn-primary' style={{ marginRight: '1rem' }}> About </button>
-                            <button className='btn btn-primary-inverse'> Contact </button>
+            <div className="main">
+                <div className="intro" id="intro">
+                    <div className="content-intro">
+                        <h1 className="title">I'M NGUYEN HUU NGHIA.</h1>
+                        <p>
+                            Software Engineer, Web Developer, JavaScript Lover & Blogger
+                            @nguyenhuunghia
+            </p>
+                        <div style={{ marginTop: "4rem" }}>
+                            <button
+                                className="btn btn-primary"
+                                style={{ marginRight: "1rem" }}
+                            >
+                                About
+                            </button>
+                            <button className="btn btn-primary-inverse"> Contact </button>
                         </div>
                     </div>
                 </div>
-                <div id='portfolio'>
-                    <h1>PORTFOLIO</h1>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, molestiae? Delectus quo dolore accusamus placeat ad suscipit enim. Asperiores minus quaerat neque et harum dolore non mollitia quas nostrum labore.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, molestiae? Delectus quo dolore accusamus placeat ad suscipit enim. Asperiores minus quaerat neque et harum dolore non mollitia quas nostrum labore.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, molestiae? Delectus quo dolore accusamus placeat ad suscipit enim. Asperiores minus quaerat neque et harum dolore non mollitia quas nostrum labore.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, molestiae? Delectus quo dolore accusamus placeat ad suscipit enim. Asperiores minus quaerat neque et harum dolore non mollitia quas nostrum labore.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, molestiae? Delectus quo dolore accusamus placeat ad suscipit enim. Asperiores minus quaerat neque et harum dolore non mollitia quas nostrum labore.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, molestiae? Delectus quo dolore accusamus placeat ad suscipit enim. Asperiores minus quaerat neque et harum dolore non mollitia quas nostrum labore.
+                <div id="portfolio" className="portfolio">
+                    <div className="section-title-block">
+                        <h1 className="section-title">
+                            <strong>PORTFOLIO</strong>
+                        </h1>
+                        <p style={{ color: "#757575" }}>Check out my lastest work below.</p>
+                    </div>
+                    <div className="card-portfolio">
+                        <div className="cell">
+                            <a
+                                href="https://gb-template.netlify.com/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img
+                                    src={port1}
+                                    width="99%"
+                                    height="auto"
+                                    alt="Gatsby Blog Template"
+                                />
+                            </a>
+                            <h2 style={{ textAlign: 'center', margin: '2rem 2rem' }}>
+                                <strong>
+                                    <a
+                                        href="https://gb-template.netlify.com/"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        Gatsby Blog Template
+                                    </a>
+                                </strong>
+                            </h2>
+                            <p style={{ color: '#757575', marginBottom: '2rem', textAlign: 'center' }}>A GatsbyJS Blog Template for blogging purpose.</p>
+                            <div className='cell-footer'>
+                                <div className="language-code">
+                                    <small>React, Create-react-app, Minimax Algorithm</small>
+                                </div>
+                                <div className='source-link'>
+                                    <a href="https://github.com/completejavascript/tic-tac-toe" target="_blank" rel="noopener noreferrer"><small>GitHub</small></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="cell">
+                            <a
+                                href="https://gb-template.netlify.com/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img
+                                    src={port1}
+                                    width="99%"
+                                    height="auto"
+                                    alt="Gatsby Blog Template"
+                                />
+                            </a>
+                            <h2 style={{ textAlign: 'center', margin: '2rem 2rem' }}>
+                                <strong>
+                                    <a
+                                        href="https://gb-template.netlify.com/"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        Gatsby Blog Template
+                                    </a>
+                                </strong>
+                            </h2>
+                            <p style={{ color: '#757575', marginBottom: '2rem', textAlign: 'center' }}>A GatsbyJS Blog Template for blogging purpose.</p>
+                            <div className='cell-footer'>
+                                <div className="language-code">
+                                    <small>React, Create-react-app, Minimax Algorithm</small>
+                                </div>
+                                <div className='source-link'>
+                                    <a href="https://github.com/completejavascript/tic-tac-toe" target="_blank" rel="noopener noreferrer"><small>GitHub</small></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="cell">
+                            <a
+                                href="https://gb-template.netlify.com/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img
+                                    src={port1}
+                                    width="99%"
+                                    height="auto"
+                                    alt="Gatsby Blog Template"
+                                />
+                            </a>
+                            <h2 style={{ textAlign: 'center', margin: '2rem 2rem' }}>
+                                <strong>
+                                    <a
+                                        href="https://gb-template.netlify.com/"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        Gatsby Blog Template
+                                    </a>
+                                </strong>
+                            </h2>
+                            <p style={{ color: '#757575', marginBottom: '2rem', textAlign: 'center' }}>A GatsbyJS Blog Template for blogging purpose.</p>
+                            <div className='cell-footer'>
+                                <div className="language-code">
+                                    <small>React, Create-react-app, Minimax Algorithm</small>
+                                </div>
+                                <div className='source-link'>
+                                    <a href="https://github.com/completejavascript/tic-tac-toe" target="_blank" rel="noopener noreferrer"><small>GitHub</small></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="cell">
+                            <a
+                                href="https://gb-template.netlify.com/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img
+                                    src={port1}
+                                    width="99%"
+                                    height="auto"
+                                    alt="Gatsby Blog Template"
+                                />
+                            </a>
+                            <h2 style={{ textAlign: 'center', margin: '2rem 2rem' }}>
+                                <strong>
+                                    <a
+                                        href="https://gb-template.netlify.com/"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        Gatsby Blog Template
+                                    </a>
+                                </strong>
+                            </h2>
+                            <p style={{ color: '#757575', marginBottom: '2rem', textAlign: 'center' }}>A GatsbyJS Blog Template for blogging purpose.</p>
+                            <div className='cell-footer'>
+                                <div className="language-code">
+                                    <small>React, Create-react-app, Minimax Algorithm</small>
+                                </div>
+                                <div className='source-link'>
+                                    <a href="https://github.com/completejavascript/tic-tac-toe" target="_blank" rel="noopener noreferrer"><small>GitHub</small></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div style={{ textAlign: 'center', marginBottom: '4rem' }}>
+                        <button
+                            className="btn btn-primary"
+                        >
+                            See more
+                        </button>
+                    </div>
                 </div>
-                <div id='about'>
+                <div id="about">
                     <h1>ABOUT</h1>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, molestiae? Delectus quo dolore accusamus placeat ad suscipit enim. Asperiores minus quaerat neque et harum dolore non mollitia quas nostrum labore.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, molestiae? Delectus quo dolore accusamus placeat ad suscipit enim. Asperiores minus quaerat neque et harum dolore non mollitia quas nostrum labore.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, molestiae? Delectus quo dolore accusamus placeat ad suscipit enim. Asperiores minus quaerat neque et harum dolore non mollitia quas nostrum labore.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, molestiae? Delectus quo dolore accusamus placeat ad suscipit enim. Asperiores minus quaerat neque et harum dolore non mollitia quas nostrum labore.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, molestiae? Delectus quo dolore accusamus placeat ad suscipit enim. Asperiores minus quaerat neque et harum dolore non mollitia quas nostrum labore.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, molestiae? Delectus quo dolore accusamus placeat ad suscipit enim. Asperiores minus quaerat neque et harum dolore non mollitia quas nostrum labore.
-                </div>
-                <div id='contact'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis,
+          molestiae? Delectus quo dolore accusamus placeat ad suscipit enim.
+          Asperiores minus quaerat neque et harum dolore non mollitia quas
+          nostrum labore. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Blanditiis, molestiae? Delectus quo dolore accusamus placeat ad
+          suscipit enim. Asperiores minus quaerat neque et harum dolore non
+          mollitia quas nostrum labore. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Blanditiis, molestiae? Delectus quo dolore accusamus
+          placeat ad suscipit enim. Asperiores minus quaerat neque et harum
+          dolore non mollitia quas nostrum labore. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Blanditiis, molestiae? Delectus quo
+          dolore accusamus placeat ad suscipit enim. Asperiores minus quaerat
+          neque et harum dolore non mollitia quas nostrum labore. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Blanditiis, molestiae?
+          Delectus quo dolore accusamus placeat ad suscipit enim. Asperiores
+          minus quaerat neque et harum dolore non mollitia quas nostrum labore.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis,
+          molestiae? Delectus quo dolore accusamus placeat ad suscipit enim.
+          Asperiores minus quaerat neque et harum dolore non mollitia quas
+          nostrum labore.
+        </div>
+                <div id="contact">
                     <h1>CONTACT</h1>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, molestiae? Delectus quo dolore accusamus placeat ad suscipit enim. Asperiores minus quaerat neque et harum dolore non mollitia quas nostrum labore.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, molestiae? Delectus quo dolore accusamus placeat ad suscipit enim. Asperiores minus quaerat neque et harum dolore non mollitia quas nostrum labore.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, molestiae? Delectus quo dolore accusamus placeat ad suscipit enim. Asperiores minus quaerat neque et harum dolore non mollitia quas nostrum labore.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, molestiae? Delectus quo dolore accusamus placeat ad suscipit enim. Asperiores minus quaerat neque et harum dolore non mollitia quas nostrum labore.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, molestiae? Delectus quo dolore accusamus placeat ad suscipit enim. Asperiores minus quaerat neque et harum dolore non mollitia quas nostrum labore.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, molestiae? Delectus quo dolore accusamus placeat ad suscipit enim. Asperiores minus quaerat neque et harum dolore non mollitia quas nostrum labore.
-                </div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis,
+          molestiae? Delectus quo dolore accusamus placeat ad suscipit enim.
+          Asperiores minus quaerat neque et harum dolore non mollitia quas
+          nostrum labore. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Blanditiis, molestiae? Delectus quo dolore accusamus placeat ad
+          suscipit enim. Asperiores minus quaerat neque et harum dolore non
+          mollitia quas nostrum labore. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Blanditiis, molestiae? Delectus quo dolore accusamus
+          placeat ad suscipit enim. Asperiores minus quaerat neque et harum
+          dolore non mollitia quas nostrum labore. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Blanditiis, molestiae? Delectus quo
+          dolore accusamus placeat ad suscipit enim. Asperiores minus quaerat
+          neque et harum dolore non mollitia quas nostrum labore. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Blanditiis, molestiae?
+          Delectus quo dolore accusamus placeat ad suscipit enim. Asperiores
+          minus quaerat neque et harum dolore non mollitia quas nostrum labore.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis,
+          molestiae? Delectus quo dolore accusamus placeat ad suscipit enim.
+          Asperiores minus quaerat neque et harum dolore non mollitia quas
+          nostrum labore.
+        </div>
             </div>
         </Layout>
     )
